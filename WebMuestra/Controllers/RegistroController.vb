add3a5a85cb2deecd1e9db0ce45d4c7d18a0c57f
johanGamaliel94 <johan.gamaliel94@gmail.com>
2017-06-21 11:04:07 -0500

    Add project files.

---
+++ WebMuestra/Controllers/RegistroController.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class RegistroController
+        Inherits Controller
+
+        ' GET: Registro
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Registro/Details/5
+        Function Details(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Registro/Create
+        Function Create() As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Registro/Create
+        <HttpPost()>
+        Function Create(ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add insert logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Registro/Edit/5
+        Function Edit(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Registro/Edit/5
+        <HttpPost()>
+        Function Edit(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add update logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Registro/Delete/5
+        Function Delete(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Registro/Delete/5
+        <HttpPost()>
+        Function Delete(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add delete logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+    End Class
+End Namespace
---
+++ WebMuestra/Models/Usuario.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data.Entity
+
+Namespace Models
+    Public Class Usuario
+        Public Property ID As Integer
+        Public Property nombreCompleto As String
+        Public Property correo As String
+    End Class
+
+    Public Class UsuarioDbContext
+        Inherits DbContext
+        Public Property Usuarios As DbSet(Of Usuario)
+    End Class
+End Namespace
